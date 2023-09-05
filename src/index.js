00d01f3f25cb1d361824a6dc611dfdf790302ed5
ellapk <mily072@naver.com>
2023-09-05 16:48:52 -0700

    feat: Update index info

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import Resolver from '@forge/resolver';
-import mysql from 'mysql2/promise'; // mysql2 패키지의 promise 모듈을 가져옴
+import mysql from 'mysql2/promise';
 
 const resolver = new Resolver();
 
 resolver.define('getText', async (req) => {
   try {
     const connection = await mysql.createConnection({
-      host: '43.202.96.57', // MySQL 호스트 주소
-      user: 'root', // MySQL 사용자 이름
-      password: '', // MySQL 비밀번호
-      database: 'gem_test' // MySQL 데이터베이스 이름
+      host: '43.202.96.57',
+      user: 'test',
+      password: '1111',
+      database: 'gem_test',
+      port: 3306
     });
 
     const [rows] = await connection.execute('SELECT brand, model FROM testtable');
